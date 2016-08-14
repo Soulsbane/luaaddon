3d95db01d5b22f54906158b59c73fb61f1a6e023
Paul Crane <paul@acheronsoft.com>
2016-08-14 02:29:44 -0500

    Add documentation for LuaAddon class.

--- source/luaaddon/luaaddon.d
+++ source/luaaddon/luaaddon.d
@@ -1,3 +1,6 @@
+/**
+	Provides a simple interface for creating a Lua addon for your program.
+*/
 module luaaddon.luaaddon;
 
 import std.algorithm : each;
@@ -5,6 +8,7 @@ import std.path : buildNormalizedPath;
 
 public import luad.all;
 
+///The main class for creating a Lua addon.
 class LuaAddon
 {
 	this()
@@ -18,6 +22,14 @@ class LuaAddon
 		writeln("Error in addon code!\n", error, "\n");
 	}
 
+	/**
+		Calls Lua function with no return value. Useful for an OnInitialize function that should be called at program start and
+		has no return value.
+
+		Params:
+			name = The name of the function to call.
+			args = The arguments to the function to call.
+	*/
 	bool callFunction(T...)(const string name, T args)
 	{
 		if(hasFunction(name))
@@ -29,29 +41,73 @@ class LuaAddon
 		return false;
 	}
 
+	/**
+		Calls a Lua function and returns its value. This should only be used with Lua functions that return only a
+		single value.
+
+		Params:
+			name = The name of the function to call.
+			args = The arguments to the function to call.
+
+		Returns:
+			The value from the Lua function that was called.
+	*/
 	auto getFunctionReturnValue(T...)(const string name, T args)
 	{
 		auto value = state_.get!LuaFunction(name)(args);
 		return value[0];
 	}
 
+	/**
+		Calls a Lua function and returns its value as a tuple
+
+		Params:
+			name = The name of the function to call.
+			args = The arguments to the function to call.
+		Returns:
+			The value as a tuple from the Lua function that was called.
+	*/
 	auto getFunctionReturnValues(T...)(const string name, T args)
 	{
 		return state_.get!LuaFunction(name)(args);
 	}
 
+	/**
+		Checks if a function is defined in the Lua addon.
+
+		Params:
+			name = Name of the function to find.
+
+		Retruns:
+			True if the function was found false otherwise.
+	*/
 	bool hasFunction(const string name)
 	{
 		return state_[name].isNil ? false : true;
 	}
 
-	alias hasTable = hasFunction; // Just to make function calls clearer.
+	/// Just makes function calls clearer.
+	alias hasTable = hasFunction;
 
+	/**
+		Creates a new table.
+
+		Params:
+			names = A list of table names to be created.
+	*/
 	void createNewTable(string[] names...)
 	{
 		names.each!(name =>	state_[name] = state_.newTable);
 	}
 
+	/**
+		Registers a function that can be called from Lua code.
+
+		Params:
+			tableName = Creates a table which the funcName will be associated with. Ex. IO.ReadText.
+			funcName = The name to use for the function on in Lua code.
+			func = The function to register.
+	*/
 	void registerFunction(T)(const string tableName, const string funcName, T func)
 	{
 		if(tableName.length)
@@ -69,11 +125,24 @@ class LuaAddon
 		}
 	}
 
+	/**
+		Registers a function that can be called from Lua code.
+
+		Params:
+			funcName = The name to use for the function on in Lua code.
+			func = The function to register.
+	*/
 	void registerFunction(T)(const string funcName, T func)
 	{
 		registerFunction(string.init, funcName, func);
 	}
 
+	/**
+		Registers a path that lua code will search for packages/modules.
+
+		Params:
+			path = A list of path names to register.
+	*/
 	void registerPackagePaths(const string[] paths...)
 	{
 		string packagePaths;
